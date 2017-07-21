02d4e3c0c353757308e4e3a2455ee621578c0bde
dickm <dickm@upmc.edu>
2017-07-21 11:43:20 -0400

    reduced flags

--- aws/emr_spark_job_submit.sh
+++ aws/emr_spark_job_submit.sh
@@ -1,9 +1,11 @@
 aws emr create-cluster \
+--name "HAIL Cluster - Spark Step (No Debug)" \
 --applications Name=Hadoop Name=Hive Name=Pig Name=Ganglia Name=Spark \
---tags 'RUN_TYPE=TRANSIENT' 'Project=HAIL' 'Environment=HAIL-DEV' \
---ec2-attributes '{"KeyName":"hail-dickm-emr-key","InstanceProfile":"EMR_EC2_DefaultRole","SubnetId":"subnet-3f61ed65","EmrManagedSlaveSecurityGroup":"sg-6a80c41b","EmrManagedMasterSecurityGroup":"sg-cf8cc8be"}' \
---release-label emr-5.7.0 --log-uri 's3n://hail-dickm-emr/emr-logs/' \
---steps '[{"Args":["spark-submit","--deploy-mode","cluster","s3://hail-dickm-emr/scripts/wordcount.py"],"Type":"CUSTOM_JAR","ActionOnFailure":"TERMINATE_CLUSTER","Jar":"command-runner.jar","Properties":"","Name":"PySpark Word Count"}]' --instance-groups '[{"InstanceCount":1,"EbsConfiguration":{"EbsBlockDeviceConfigs":[{"VolumeSpecification":{"SizeInGB":32,"VolumeType":"gp2"},"VolumesPerInstance":1}]},"InstanceGroupType":"CORE","InstanceType":"m4.large","Name":"Core - 2"},{"InstanceCount":1,"EbsConfiguration":{"EbsBlockDeviceConfigs":[{"VolumeSpecification":{"SizeInGB":32,"VolumeType":"gp2"},"VolumesPerInstance":1}]},"InstanceGroupType":"MASTER","InstanceType":"m4.large","Name":"Master - 1"}]' \
---auto-terminate --auto-scaling-role EMR_AutoScaling_DefaultRole --ebs-root-volume-size 10 --service-role EMR_DefaultRole --enable-debugging \
---name 'HAIL Initial Cluster (Spark Step)' \
---scale-down-behavior TERMINATE_AT_INSTANCE_HOUR --region us-east-1
+--ec2-attributes KeyName=hail-dickm-emr-key,InstanceProfile=EMR_EC2_DefaultRole \
+--ami-version emr-5.6.0 --log-uri s3://hail-dickm-emr/emr-logs/ \
+--steps Type=CUSTOM_JAR,ActionOnFailure=TERMINATE_CLUSTER,Jar=command-runner.jar,Name="PySpark Word Count", \
+Args=[spark-submit,--master,yarn,--deploy-mode,cluster,s3://hail-dickm-emr/scripts/wordcount.py] \
+--instance-groups \
+InstanceGroupType=MASTER,InstanceCount=1,InstanceType=m4.large \
+InstanceGroupType=CORE,InstanceCount=1,InstanceType=m4.large \
+--auto-terminate --region us-east-1 --no-verify-ssl
